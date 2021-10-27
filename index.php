c3482cc937a515c1b545db440286785c46efade2
Julio Oliveira <sistemaprecisao@gmail.com>
2021-10-27 10:47:40 -0400

    criando index e script php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="urf-8">
+    <title>Formulário de inscrição</title>
+    <meta name="author" content="">
+    <meta name="description" content="">
+    <meta name="viewport" content="width-device-width, initial-scale=1">
+</head>
+
+<body>
+<p>FORMULÁRIO PARA INSCRIÇÃO DE COMPETIDORES</p>
+
+<form action="script.php" method="post">
+    <p>Seu nome: <input type="text" name="nome"/> </p>
+    <p>Sua idade: <input type="text" name="idade"/> </p>
+    <p><input type="submit"/> </p>
+</form>
+
+</body>
+
+</html>
---
+++ script.php
@@ -0,0 +1,23 @@
+<?php
+
+$categorias = [];
+$categorias[] = 'infantil';
+$categorias[] = 'adolescente';
+$categorias[] = 'adulto';
+$categorias[] = 'idoso';
+
+var_dump($nome);
+var_dump($idade);
+
+
+$nome = $_POST['nome'];
+$idade = $_POST['idade'];
+
+
+
+/*if($idade >= 0 && $idade <=12){
+
+
+}
+*/
+?>
